528d91fc3706a709ae36a091191f40b2840a5c8b
Irawan <irawan@live.com.au>
2026-07-10 06:44:41 +0000

    updated content in fuel calculator

---
+++ fuel-calc-share.js
@@ -0,0 +1,3 @@
+<script>
+document.write("<iframe id=\"fuel-calc-iframe\" src=\"https:\/\/northernterritory.com\/fuel-calculator.html\" width=\"100%\" style=\"border:0; min-height:1000px;\" loading=\"lazy\" title=\"Northern Territory Fuel Cost Calculator\"><\/iframe>\n");
+</script>
